7c5f4a259662461f440f073ccce0baf4abae343d
Rose210 <84668996+Rose210@users.noreply.github.com>
2021-05-23 19:13:59 +0530

    Add files via upload
    The fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+n=int(input("Enter the  no. of terms"))
+a=0                                         #first element of series
+b=1
+c,i
+print(a,",",b,",")
+for i in range 2                                    #second element of series
+    c=a+b
+    print("The fibonacci series in sequence:", c," , ")
+    a=b
+    b=c
+    i=i+1
