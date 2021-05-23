6aaf7eb0023b04f8bc2a8ee3ff2aa457c51f90dc
Rose210 <84668996+Rose210@users.noreply.github.com>
2021-05-23 20:24:04 +0530

    Add files via upload
    Print only positive number in the list

---
+++ pve.py
@@ -0,0 +1,16 @@
+myList_1=[12,-7,5,64,-14]
+myList_2=[12,14,-95,3]
+
+i=0
+print ("List_1=[12,-7,5,64,-14]")
+for i in range (5) :
+
+    if myList_1[i]>0:
+        print(myList_1[i] )
+        i=i+1
+print("List_2=[12,14,-95,3]")
+for i in range (4):
+
+    if myList_2[i]>0:
+        print(myList_2[i] )
+        i=i+1
